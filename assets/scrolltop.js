b5ecc4a98c1b0c869f08caf73727f9631248d985
Abhishek Manandhar <a.manandhar@buildingradar.com>
2018-11-06 00:21:30 +0100

    New layout

--- assets/scrolltop.js
+++ assets/scrolltop.js
@@ -9,23 +9,24 @@ var navElement = document.getElementById("main-nav");
 var logo = document.getElementById("layer101");
 
 // Get the offset position of the navbar
-var sticky = 60 * window.innerHeight/100; //50vh
+var sticky = 60 * window.innerHeight/100; //60vh
+var nosticky = 2 * window.innerHeight/100; //5vh
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     navElement.classList.add("fixed-top");
     navElement.classList.add("bg-light");
-    navElement.classList.add("shadow");
     navElement.classList.remove("navbar-dark");
     navElement.classList.remove("bg-dark");
+    navElement.classList.remove("position-absolute");
     logo.setAttribute("fill", "darkslateblue");
   } else {
     navElement.classList.remove("fixed-top");
-    navElement.classList.remove("shadow");
     navElement.classList.remove("bg-light");
     navElement.classList.add("navbar-dark");
     navElement.classList.add("bg-dark");
+    navElement.classList.add("position-absolute");
     logo.setAttribute("fill", "white");
   }
 }
