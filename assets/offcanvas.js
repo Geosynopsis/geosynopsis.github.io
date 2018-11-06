d88297665245a0a7cde63021dfe48d52b18509a1
Abhishek Manandhar <a.manandhar@buildingradar.com>
2018-11-06 22:46:17 +0100

    New layout

---
+++ assets/offcanvas.js
@@ -0,0 +1,10 @@
+/**
+ * Created by abheeman on 06.11.18.
+ */
+$(function () {
+  'use strict';
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
